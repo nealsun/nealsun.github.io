6483b2abac82fb36fcb4b1b812a5de1ee1109aad
sunweirong <hzsunweirong@corp.netease.com>
2017-01-06 13:26:01 +0800

    替换

---
+++ js/main.js
@@ -0,0 +1,4 @@
+/*! PHOTOSYNTHETIC 2017-01-06 */
+
+$(function(){if(window.Swiper){new Swiper(".swiper-container",{direction:"horizontal",autoplay:5e3,speed:1e3,loop:!0,pagination:".swiper-pagination",nextButton:".swiper-button-next",prevButton:".swiper-button-prev"})}});
+//# sourceMappingURL=main.js.map
